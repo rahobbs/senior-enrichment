7cb4ada5e7ba2f44f386a8f7510a5a1f04848d22
rachael <hobbs.rachael@gmail.com>
2016-10-28 13:45:09 -0400

    updates to routes

--- server/api.js
+++ server/api.js
@@ -16,36 +16,47 @@ api.get('/posts', function(req, res, next) {
       console.log(foundPost);
       res.send(foundPost);
     }
-  }).catch(next);
+  }).catch(next)
 })
 
 //Get single post based on ID
 api.get('/posts/:id', function(req, res, next) {
   Post.findById(req.params.id).then(function(foundPost) {
-    if(!foundPost) {
+    if (!foundPost) {
       res.sendStatus(404);
     } else {
       console.log(foundPost);
       res.send(foundPost);
     }
   })
+  .catch(next)
 })
 //Create a new post
 api.post('/posts', function(req, res, next) {
   Post.create(req.body).then(function(newPost) {
     res.send(newPost);
-  })
+  }).catch(next)
 })
 //Create a new comment on a given post
 api.post('/comments/:postId', function(req, res, next) {
-
+  Comment.create({content: req.body.content, postId: req.params.postId})
+  .then(function(newComment) {
+    res.send(newComment);
+  }).catch(next)
 })
 //Delete a given post and all associated comments
 api.delete('posts/:id', function(req, res, next) {
-
+  Post.destroy({where: {id: req.params.id}}).then(function() {
+    Comment.destroy({where: {postId: req.params.id}})
+  }).then(function() {
+    res.sendStatus(200);
+  })
+  .catch(next);
 })
 //Delete a given comment
 api.delete('comments/:id', function(req, res, next) {
-
+  Comment.destroy({where: {id: req.params.id}}).then(function() {
+    res.sendStatus(200);
+  }).catch(next)
 })
 module.exports = api
